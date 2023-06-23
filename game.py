a1bb323adede44039c6c7a5c54e64614f1db3747
MrTup <lookchunkiu@gmail.com>
2023-06-23 10:36:52 +0100

    background

--- game.py
+++ game.py
@@ -1,37 +1,40 @@
-import pygame 
+import pygame
 
 pygame.init()
 
-#variables
-BLACK    = (   0,   0,   0)
-WHITE    = ( 255, 255, 255)
-GREEN    = (   0, 255,   0)
-RED      = ( 255,   0,   0)
-BLUE     = (   0,   0, 255)
+screenWidth = 400
+screenHeight = 600
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
-#game window
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 600
+#create game window
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+pygame.display.set_caption('DungeonDestroyers')
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Dungeon Destroyers')
+#load images
+backgroundImg = pygame.image.load('assets/bg.png').convert_alpha()
 clock = pygame.time.Clock()
 
 run = True
 while run == True:
-    #event handler for key strokes
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False 
+	#event handler
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run = False
+			
+    #game logic		
 
-    #clears screen and set background        
-    screen.fill(WHITE)
+	#draw background
+	screen.blit(backgroundImg, (0, 0))
+	
+    #draw objects
+	pygame.draw.rect(screen, RED, [55,50,20,25])   
+	
+	#update display window
+	pygame.display.update()
+	clock.tick(60)
 
-    #drawing code
-    pygame.draw.rect(screen, RED, [55, 50, 20, 25], 0)
-    #updates screen with drawing code
-    pygame.display.flip()
-
-    #60 updates a second
-    clock.tick(60)
+	
 pygame.quit()
