09764535661d9f0e14f1f6e7e3cfdfd1c0dfc5c7
MrTup <lookchunkiu@gmail.com>
2023-06-24 23:36:25 +0100

    Gravity and Jump

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-a = 0
+GRAVITY = 0.4
 #create game window
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption('DungeonDestroyers')
@@ -25,24 +25,33 @@ class Player():
 		self.image = pygame.transform.scale(playerImg, (50,50)) 
 		self.width = 38
 		self.height = 46
+		self.vel_y = 0
 		self.rect = pygame.Rect(0, 0, self.width, self.height)
 		self.rect.center = (x,y)
 	
 	def move(self):
 		dx = 0
 		dy = 0
-
+		self.vel_y += GRAVITY
 		key = pygame.key.get_pressed()
 		if key[pygame.K_a]: 
 			dx = -10
 		if key[pygame.K_d]:
 			dx = 10
+		if key[pygame.K_w]:
+			self.vel_y = -10
+
+		dy += self.vel_y
 
 		if (self.rect.left +dx <0 or self.rect.right + dx >= screenWidth):
-			pass
-		else :
-			self.rect.x += dx 
-			self.rect.y += dy
+			dx =0
+
+		if self.rect.bottom > screenHeight:
+			dy = 0
+
+		self.rect.x += dx 
+		self.rect.y +=dy
+
 
 	def draw(self):
 		screen.blit(self.image, (self.rect.x - 6, self.rect.y-4))
