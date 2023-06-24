41a05e203a54b76f768d04ed938f1d00ded222f1
MrTup <lookchunkiu@gmail.com>
2023-06-24 22:01:52 +0100

    added player

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ pygame.init()
 
 screenWidth = 400
 screenHeight = 600
+
+#colours
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
@@ -15,7 +17,30 @@ pygame.display.set_caption('DungeonDestroyers')
 
 #load images
 backgroundImg = pygame.image.load('assets/bg.png').convert_alpha()
+playerImg = pygame.image.load('assets/jump.png').convert_alpha()
 clock = pygame.time.Clock()
+
+#classes
+class Player():
+	def __init__(self,x,y):
+		self.image = pygame.transform.scale(playerImg, (50,50)) 
+		self.width = 38
+		self.height = 46
+		self.rect = pygame.Rect(0, 0, self.width, self.height)
+		self.rect.center = (x,y)
+	
+	def move(self):
+		key = pygame.key.get_pressed()
+		if key[pygame.K_a]: 
+			self.rect.x -= 10
+		if key[pygame.K_d]:
+			self.rect.x +=10
+
+	def draw(self):
+		screen.blit(self.image, (self.rect.x - 6, self.rect.y-4))
+		pygame.draw.rect(screen, RED, self.rect, 2)
+
+player1 = Player(screenWidth // 2, screenHeight //2)
 
 run = True
 while run == True:
@@ -24,17 +49,16 @@ while run == True:
 		if event.type == pygame.QUIT:
 			run = False
 			
-    #game logic		
-
-	#draw background
+  #game logic		
+	player1.move()
+	#images
 	screen.blit(backgroundImg, (0, 0))
 	
-    #draw objects
-	pygame.draw.rect(screen, RED, [55,50,20,25])   
-	
+  #draw objects
+	player1.draw()
+
 	#update display window
 	pygame.display.update()
 	clock.tick(60)
-
 	
 pygame.quit()
