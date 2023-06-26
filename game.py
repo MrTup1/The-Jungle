46d55e73699504ca41401aeda72860fa7491d50a
MrTup <lookchunkiu@gmail.com>
2023-06-26 14:29:58 +0100

    infinite background

--- game.py
+++ game.py
@@ -1,25 +1,33 @@
 import pygame
 import time
+import math
 
 pygame.init()
 
-screenWidth = 400
+screenWidth = 2000
 screenHeight = 600
 
-#colours
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-GRAVITY = 0.4
 #create game window
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption('DungeonDestroyers')
 
 #load images
 backgroundImg = pygame.image.load('assets/bg.png').convert_alpha()
+backgroundImg_width = backgroundImg.get_width() 
 playerImg = pygame.image.load('assets/jump.png').convert_alpha()
 clock = pygame.time.Clock()
+
+
+#variables
+scroll = 0
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+GRAVITY = 0.4
+tiles = math.ceil((screenWidth / backgroundImg_width))
+print(tiles)
+
 #classes
 class Player():
 	def __init__(self,x,y):
@@ -38,22 +46,22 @@ class Player():
 		dy = 0
 		self.vel_y += GRAVITY
 		key = pygame.key.get_pressed()
-		if key[pygame.K_a]: 
-			dx = -10
-		if key[pygame.K_d]:
-			dx = 10
+		if key[pygame.K_LEFT] or key[pygame.K_a]: 
+			dx = -5
+		if key[pygame.K_RIGHT] or key[pygame.K_d]:
+			dx = 5
 		if self.landed == False:
 			pass 
 		else: 			
-			if key[pygame.K_w]:
+			if key[pygame.K_w] or key[pygame.K_UP]:
 					self.landed = False
 					self.vel_y = -12
-					self.start = time.time()
-		
+					self.start = time.time()	
 
 		dy += self.vel_y
 
 		if (self.rect.left +dx <0 or self.rect.right + dx >= screenWidth):
+			screen.blit(backgroundImg, (screenWidth , 0))
 			dx =0
 
 		if self.rect.bottom +dy > screenHeight:
@@ -68,6 +76,8 @@ class Player():
 		screen.blit(self.image, (self.rect.x - 6, self.rect.y-4))
 		pygame.draw.rect(screen, RED, self.rect, 2)
 
+
+#Creating Objects
 player1 = Player(screenWidth // 2, screenHeight -50)
 
 run = True
@@ -80,13 +90,16 @@ while run == True:
   #game logic		
 	player1.move()
 	#images
-	screen.blit(backgroundImg, (0, 0))
+	for i in range(tiles):
+		screen.blit(backgroundImg, (i * backgroundImg_width, 0))
 	
+	#scroll background
+
   #draw objects
 	player1.draw()
 
 	#update display window
 	pygame.display.update()
-	clock.tick(60)
+	clock.tick(120)
 	
 pygame.quit()
