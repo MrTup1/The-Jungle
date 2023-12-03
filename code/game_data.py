0bd511b0ae4760af8553597d97d55b26d95c94ac
MrTup <lookchunkiu@gmail.com>
2023-12-03 11:35:54 +0000

    Level 3

--- code/game_data.py
+++ code/game_data.py
@@ -29,14 +29,14 @@ level_1 = {
 }
 
 level_2 = {
-    'terrain' : './levels/0/level_0_terrain.csv',
-    'coins' : './levels/0/level_0_coins.csv',
-    'fg palms' : './levels/0/level_0_fg_palms.csv',
-    'bg palms' : './levels/0/level_0_bg_trees.csv',
-    'leaves' : './levels/0/level_0_bg_leaves.csv',
-    'opossum' : './levels/0/level_0_enemies.csv',
-    'constraint' : './levels/0/level_0_constraints.csv',
-    'player' : './levels/0/level_0_player.csv',
+    'terrain' : './levels/2/level_2_terrain.csv',
+    'coins' : './levels/2/level_2_coins.csv',
+    'fg palms' : './levels/2/level_2_fg_palms.csv',
+    'bg palms' : './levels/2/level_2_bg_trees.csv',
+    'leaves' : './levels/2/level_2_bg_leaves.csv',
+    'opossum' : './levels/2/level_2_opossum.csv',
+    'constraint' : './levels/2/level_2_constraints.csv',
+    'player' : './levels/2/level_2_player.csv',
     'nodePos':(420, 410),
     'content': 'this is level 2',
     'unlock' : 3,
