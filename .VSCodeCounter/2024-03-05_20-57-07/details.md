0d0521f2174e4ec74a35ddc9d5944dfaefabbe34
MrTup <lookchunkiu@gmail.com>
2024-03-18 16:52:27 +0000

    Player comments

---
+++ .VSCodeCounter/2024-03-05_20-57-07/details.md
@@ -0,0 +1,30 @@
+# Details
+
+Date : 2024-03-05 20:57:07
+
+Directory c:\\Users\\lookc\\Documents\\GitHub\\The-Jungle\\code
+
+Total : 15 files,  1086 codes, 29 comments, 228 blanks, all 1343 lines
+
+[Summary](results.md) / Details / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
+
+## Files
+| filename | language | code | comment | blank | total |
+| :--- | :--- | ---: | ---: | ---: | ---: |
+| [code/classes/camera.py](/code/classes/camera.py) | Python | 37 | 0 | 11 | 48 |
+| [code/classes/level.py](/code/classes/level.py) | Python | 245 | 10 | 58 | 313 |
+| [code/classes/opossum.py](/code/classes/opossum.py) | Python | 23 | 0 | 7 | 30 |
+| [code/classes/platform.py](/code/classes/platform.py) | Python | 11 | 0 | 2 | 13 |
+| [code/classes/player.py](/code/classes/player.py) | Python | 195 | 5 | 37 | 237 |
+| [code/classes/start.py](/code/classes/start.py) | Python | 32 | 3 | 3 | 38 |
+| [code/classes/tile.py](/code/classes/tile.py) | Python | 57 | 0 | 13 | 70 |
+| [code/classes/ui.py](/code/classes/ui.py) | Python | 67 | 4 | 16 | 87 |
+| [code/functions/background.py](/code/functions/background.py) | Python | 29 | 0 | 7 | 36 |
+| [code/functions/button.py](/code/functions/button.py) | Python | 18 | 0 | 7 | 25 |
+| [code/functions/support.py](/code/functions/support.py) | Python | 49 | 0 | 12 | 61 |
+| [code/game.py](/code/game.py) | Python | 80 | 4 | 16 | 100 |
+| [code/game_data.py](/code/game_data.py) | Python | 93 | 0 | 6 | 99 |
+| [code/overworld.py](/code/overworld.py) | Python | 115 | 0 | 25 | 140 |
+| [code/settings.py](/code/settings.py) | Python | 35 | 3 | 8 | 46 |
+
+[Summary](results.md) / Details / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
--- code/classes/player.py
+++ code/classes/player.py
@@ -19,8 +19,8 @@ class Player(pygame.sprite.Sprite):
 		self.currentLevel = currentLevel
 
 		#player status
-		self.status = "idle"
-		self.facing = "right"
+		self.status = "idle" #For determing what sprite to draw
+		self.facing = "right" #For flipping sprite
 		self.onGround = False
 		self.onGroundTime = time.time()
 		self.opossumVulnerability = False
@@ -46,7 +46,7 @@ class Player(pygame.sprite.Sprite):
 		self.maxJumps = 2
 		self.jumpsRemaining = 2
 
-		self.changeHealth = changeHealth
+		self.changeHealth = changeHealth #Get change health method from game class
 		self.invincible = False
 		self.inivincibleDuration = 400
 		self.hurtTime = 0
@@ -73,13 +73,13 @@ class Player(pygame.sprite.Sprite):
 
 		for animation in self.animations.keys():
 			fullPath = characterPath + animation
-			self.animations[animation] = importFolder(fullPath)
+			self.animations[animation] = importFolder(fullPath) #get an array of images for each key(status) in the dictionary
 
-	def animate(self):
-		animation = self.animations[self.status]
+	def animate(self):#Animate player sprite
+		animation = self.animations[self.status] #Get collection of images based on player status
 
-		self.frameIndex += self.animationSpeed
-		if self.frameIndex >= len(animation):
+		self.frameIndex += self.animationSpeed #Increment frame index
+		if self.frameIndex >= len(animation): #Reset frame index when animation frame is at its last image
 			self.frameIndex = 0
 
 		image = animation[int(self.frameIndex)]
@@ -87,17 +87,17 @@ class Player(pygame.sprite.Sprite):
 			self.image = image
 			self.rect.bottomleft = self.collisionRect.bottomleft
 		else:
-			flippedImg = pygame.transform.flip(image, True, False)
+			flippedImg = pygame.transform.flip(image, True, False) #Flipe image (default facing towards right)
 			self.image = flippedImg
 			self.rect.bottomright = self.collisionRect.bottomright
 
-		if self.invincible:
-			alpha = self.flicker()
-			self.image.set_alpha(alpha)
+		if self.invincible: #If player is invicinible (after damaged)
+			alpha = self.flicker() #Alpha value
+			self.image.set_alpha(alpha) #Set image to specific alpha value (Opaque or transparent)
 		else:
-			self.image.set_alpha(255)
+			self.image.set_alpha(255) #255 is fully opaque
 
-	def checkUnlocked(self):
+	def checkUnlocked(self): #Logic for obtaining if player can perform a specific ability (based on level)
 		if self.currentLevel > 1:
 			self.unlockedDash = True
 		if self.currentLevel > 2:
@@ -116,7 +116,7 @@ class Player(pygame.sprite.Sprite):
 			else:
 				self.direction.x = 0
 
-			if self.unlockedDouble: #If unlocked double jump
+			if self.unlockedDouble: #If player has unlocked double jump
 				if keys[pygame.K_z]: #JUMP
 					self.time += 1
 					
@@ -125,10 +125,10 @@ class Player(pygame.sprite.Sprite):
 								self.jump()
 
 					if not self.jumped: #If player has jumped 1st time
-						self.jumped = True 
+						self.jumped = True #self.jumped stores if player has jumped one time, set it to true
 						self.jumpsRemaining -= 1 
 
-					if not self.doubleJumped and self.jumped and self.releasedJump == True: #Check if first jump is completed
+					if not self.doubleJumped and self.jumped and self.releasedJump == True: #Check if first jump is completed, to jump a 2nd time
 						self.doubleJumped = True	
 
 				elif keys[pygame.K_z] == False:
@@ -137,7 +137,7 @@ class Player(pygame.sprite.Sprite):
 							self.releasedJump = True #After 1st release of key from jump
 						if self.doubleJumped:  #After 2nd release of key from jump
 							self.jumpsRemaining -= 1 
-			else:	#Jump when double jump is still locked			
+			else:	#Jump function when double jump is still locked			
 				if keys[pygame.K_z]:
 					self.time += 1
 					if self.time < 16 and self.onCeiling == False and self.releasedJump == False:
@@ -146,8 +146,8 @@ class Player(pygame.sprite.Sprite):
 						self.time = 0
 						self.releasedJump = True
 		
-			if self.unlockedDash:
-				if keys[pygame.K_c] and self.finalDashed == False: #DASH
+			if self.unlockedDash:#Check if dash ability unlocked
+				if keys[pygame.K_c] and self.finalDashed == False: 
 					self.dashed = True
 				
 				if self.dashed == True and self.finalDashed == False:
@@ -157,7 +157,7 @@ class Player(pygame.sprite.Sprite):
 						self.dashFunction(-1)
 
 
-	def getStatus(self):
+	def getStatus(self): #Get player attributes related to status, used to determine player sprite to display 
 		if self.direction.y < 0:
 			self.status = "jump"
 		elif self.direction.y > 0.8 and self.releasedJump == True:
@@ -170,8 +170,8 @@ class Player(pygame.sprite.Sprite):
 			
 			if self.finalDashed:
 				currentTime = pygame.time.get_ticks()
-				if currentTime - self.dashedTime >= self.dashCooldown:
-					self.dashed = False
+				if currentTime - self.dashedTime >= self.dashCooldown: #If time after dash is larger than cooldown
+					self.dashed = False #Reset all attributes related to dash
 					self.finalDashed = False
 					self.dashCounter = 0
 			if self.direction.x != 0:
@@ -179,39 +179,39 @@ class Player(pygame.sprite.Sprite):
 			else:
 				self.status = "idle"
 		
-	def getDamage(self):
+	def getDamage(self): #Damage player
 		if not self.invincible:
 			self.changeHealth(-1) 
 			self.invincible = True
-			self.hurtTime = pygame.time.get_ticks()\
+			self.hurtTime = pygame.time.get_ticks() 
 
-	def dashFunction(self, direction):
-		if self.dashed == True and self.dashCounter < 6:
-			self.dashCounter +=1
-			self.dash(direction)
+	def dashFunction(self, direction): #Dictates how long dash lasts for
+		if self.dashed == True and self.dashCounter < 6: #If player has already pressed c, and less than a certain distances
+			self.dashCounter +=1 #Increment counter, so it can break above
+			self.dash(direction) 
 			if self.dashCounter > 5: 
-				self.finalDashed = True
+				self.finalDashed = True #Stops dash if more than a certain distance
 				self.dashedTime = pygame.time.get_ticks()
 
-	def dash(self, direction):
+	def dash(self, direction): #Actual translation of player when dashing
 		if self.paused == False:
-			self.direction.y = 0
-			self.direction.x = 5 * direction
+			self.direction.y = 0 #Prevent player from falling by gravity
+			self.direction.x = 5 * direction #Move forwards by 5 pixels every time dash is called 
 	
 
 	def inivincibleTime(self):
 		if self.invincible:
 			currentTime = pygame.time.get_ticks()
-			if currentTime - self.hurtTime >= self.inivincibleDuration: 
+			if currentTime - self.hurtTime >= self.inivincibleDuration: #Compare if time since damage is more than cooldown time (400ms)
 				self.invincible = False
 
-	def flicker(self):
-		value = sin(pygame.time.get_ticks())
-		if value >= 0: return 0
-		else: return 255
+	def flicker(self): #Changes alpha value of player
+		value = sin(pygame.time.get_ticks()) #value varies from -1 to 1 like sin wave, so it varies constantly when called
+		if value >= 0: return 0 #0 represents transparent
+		else: return 255 #255 respresents fully opaque
 
 	def applyGravity(self):
-		self.direction.y += self.gravity
+		self.direction.y += self.gravity #Translate player donwards exponentially 
 		self.collisionRect.y += self.direction.y
 	
 	def jump(self):
@@ -219,12 +219,12 @@ class Player(pygame.sprite.Sprite):
 	
 	
 	def accelerate(self, direction):
-		if self.direction.x + self.acceleration * direction <= 1.25 and direction == 1 or self.direction.x + self.acceleration * direction >= -1.25 and direction == -1:
-			self.direction.x += self.acceleration * direction
+		if self.direction.x + self.acceleration * direction <= 1.25 and direction == 1 or self.direction.x + self.acceleration * direction >= -1.25 and direction == -1: #Check if player is moving slower than maximum velocity
+			self.direction.x += self.acceleration * direction #Accelerate player
 			self.collisionRect.x += self.direction.x *direction
-		else:
+		else: #If maximum velocity is reached
 			if direction == 1:
-				self.direction.x = 1.25
+				self.direction.x = 1.25 #Player moves at constant velociuty
 			else: 
 				self.direction.x = -1.25
 
